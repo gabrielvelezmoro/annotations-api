9aa60ef4ceb36e6719a9b1abdaffbfbdd3ad290a
gabri <gabrieuvelezmoro@gmail.com>
2023-04-30 00:54:42 -0300

    test: testa criação de nota

--- src/shared/infra/http/routes/operation/anotacao-routes.spec.ts
+++ src/shared/infra/http/routes/operation/anotacao-routes.spec.ts
@@ -1,22 +1,49 @@
 import request from "supertest";
-
+import { Connection, getConnection } from 'typeorm';
 import { app } from "../../../../infra/http/app";
+import createConnection from '@shared/infra/typeorm';
 
+let connection: Connection;
 let token;
 
 describe("Create Anotacao Controller Integration Test", () => {
-  it("should be able to create anotação", async () => {
-    
-    const response = await request(app).post("/notas/").send({
-        "titulo": "Salve",
-        "descricao": "salve salve",
-        "idPessoa": 2
+  beforeAll(async () => {
+    connection = await createConnection();
+  });
+  it("should be able to create a user", async () => {
+    const response = await request(app).post("/users").send({
+      name: "gabriel_teste",
+	    email: "teste_gabriel@gmail.com",
+	    password: "admin"
     });
 
 
-    console.log(response)
-
     expect(response.status).toBe(201);
+  });
+
+  it("should be able to authenticate a user", async () => {
+    const response = await request(app).post("/sessions").send({
+      email: "teste_gabriel@gmail.com",
+      password: "admin",
+    });
+
+    token = response.body.token;
+
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it("should be able to create anotação", async () => {
+    
+    const response = await request(app).post("/notas/")
+      .set({ Authorization: `Bearer ${token}` })
+      .send({
+          titulo: "Salve",
+          descricao: "salve salve",
+          idPessoa: 2
+      });
+
+
+    expect(response.status).toBe(200);
   });
 
 });
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,10 +5,7 @@ export default async (): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      database:
-        process.env.NODE_ENV === 'test'
-          ? 'security_test'
-          : defaultOptions.database,
+      database: defaultOptions.database,
     })
   )
 }
